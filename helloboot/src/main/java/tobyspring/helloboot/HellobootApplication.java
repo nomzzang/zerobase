7ae08702a17f808aa16a5a4c04eff5bddadef5da
kyuzzang <starnomzzang@naver.com>
2023-05-01 21:37:23 +0900

    섹션5까지 완료

---
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,28 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+@ComponentScan
+//@SpringBootApplication
+public class HellobootApplication {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory(){
+        return new TomcatServletWebServerFactory();
+    }
+    @Bean public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+    public static void main(String[] args) {
+        //컨테이너를 하는 작업 빈 초기화
+        SpringApplication.run(HellobootApplication.class, args);
+    }
+}
