f1ee896a79203b03dcbabff153f59cdb36e05b47
kyuzzang <starnomzzang@naver.com>
2023-05-02 10:11:19 +0900

    섹션6

--- helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ helloboot/src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -3,6 +3,23 @@ package tobyspring.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+
+}
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest{
+
+}
 public class HelloServiceTest {
     @Test
     void simpleHelloService(){
@@ -13,9 +30,10 @@ public class HelloServiceTest {
         Assertions.assertThat(ret).isEqualTo("sayHelloTest");
     }
 
+    @Test
     void helloDecorator() {
         HelloDecorator decorator = new HelloDecorator(name -> name);
-        String ret = decorator.sayHello("test");
+        String ret = decorator.sayHello("Test");
         Assertions.assertThat(ret).isEqualTo("**Test**");
 
     }
