1e387a7bdbae11fa94c7dddb5e9711215f513391
kyuzzang <starnomzzang@naver.com>
2023-05-03 14:10:48 +0900

    섹션8

---
+++ helloboot/src/main/java/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package config.autoconfig;
+
+import config.ConditionalMyOnClass;
+import config.EnableMyConfigurationProperties;
+import config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)
+public class DataSourceConfig {
+    @Bean
+    DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+}
